fe55d5356e938421c622e6c6ed4f87a18b8b6936
OmarDaSilva <72036965+OmarDaSilva@users.noreply.github.com>
2023-02-26 18:55:45 +0000

    sorted commits by day and authors

--- gitAnalysis.js
+++ gitAnalysis.js
@@ -1,83 +1,229 @@
-import NodeGit from "nodegit"
-import eventsEmitter from "./EventEmitter.js"
+import NodeGit from "nodegit";
+import eventsEmitter from "./EventEmitter.js";
 
 var index = 0;
 
-export default async function gitAnalysis(repoFilepath, branch) {
+export default async function gitAnalysis(repoFilepath, branchName) {
   const repo = await NodeGit.Repository.open(repoFilepath);
-  const masterBranch = await repo.getMasterCommit();
+  const branches = await getAllBranches(repo);
 
-  const masterBrancHistory = masterBranch.history(NodeGit.Revwalk.SORT.TIME);
+  // const branch = await repo.getBranch(test);
+  const branch = branchName
+    ? await repo.getBranch(branchName)
+    : await repo.getBranch(branches.main);
 
-  composeFile(masterBrancHistory);
-  masterBrancHistory.start();
+  await composeJSONFile(repo, branch);
+
+  // const masterBranch = await repo.getMasterCommit();
+
+  // const masterBrancHistory = masterBranch.history(NodeGit.Revwalk.SORT.TIME);
+
+  // composeFile(masterBrancHistory);
+  // masterBrancHistory.start();
 }
 
-async function composeFile(branchEventEmitter) {
-  const contributors = {};
-   /* 
-      This function gets all the commits and then creates an author object that stores information about which
-      files this contributor has changed up to the most recent commit
-    */
-  branchEventEmitter.on("commit", async (commit) => {
-    let authorName = commit.committer().name();
+async function getAllBranches(repo) {
+  const refs = await repo.getReferences();
+  const mainOrMaster = [
+    refs.find(
+      (branches) =>
+        branches.name().includes("main") || branches.name().includes("master")
+    ),
+  ];
 
-    if (contributors[authorName] == undefined) {
-      contributors[authorName] = {
-        contributions: 0,
-        files: [],
+  return {
+    branches: refs,
+    main: mainOrMaster[0],
+  };
+}
+
+async function composeJSONFile(repo, branch) {
+  // const commitsByDay = {};
+  const headCommit = await repo.getBranchCommit(branch);
+
+  const revWalk = repo.createRevWalk();
+  revWalk.sorting(NodeGit.Revwalk.SORT.REVERSE);
+  revWalk.push(headCommit.id());
+
+  const commits = await revWalk.getCommitsUntil((_commit) => true);
+  await composeData(commits);
+
+  // TODO: Rewrite compose file to take the commits array and process it
+}
+
+async function composeData(commits) {
+  const commitsByDay = {};
+
+  commits.forEach(async (commit) => {
+    const commitDate = new Date(commit.date());
+    // Reset the time to midnight to group by day
+    commitDate.setHours(0, 0, 0, 0);
+    const dateString = commitDate.toISOString();
+
+    // Get Authors name
+    const authorName = commit.committer().name();
+
+    if (commitsByDay[dateString] == undefined) {
+      commitsByDay[dateString] = {
+        contributors: {},
+        repoState: {},
       };
-
-      let currentCommitTreeEntires = await commit.getTree();
-      let filenames = currentCommitTreeEntires.entries()
-      
-      // potential duplicating code
-      filenames.forEach(async (entry) => {
-        if (entry.isFile()) {
-          contributors[authorName].files.push({
-            entryName: entry.name(),
-            isDirectory: false,
-            children: null
-          })
-        } else {
-          contributors[authorName].files.push({
-            entryName: entry.name(),
-            isDirectory: true,
-            children: await getDirectoryEntries(entry)
-          })
-        }
-      })    
     }
 
+    if (commitsByDay[dateString].contributors[authorName] == undefined) {
+      commitsByDay[dateString].contributors[authorName] = {
+        filesChanged: [],
+      };
+    }
+
+    const diffArray = await commit.getDiff();
+    if (diffArray.length == 1) {
+      const diff = diffArray[0];
+      const patches = await diff.patches();
+      const modifiedFiles = [];
+      
+
+      for (const patch of patches) {
+        const newFile = patch.newFile().path();
+
+        if (newFile) {
+          commitsByDay[dateString].contributors[authorName].filesChanged.push(
+            newFile
+          );
+        }
+      }
+    }
+
+    /* 
+      This function below gets the current state of our Repo,
+      Todo: We need to make sure we include 
+    */
+
+    let currentCommitTreeEntires = await commit.getTree();
+    let filenames = currentCommitTreeEntires.entries();
+
+    filenames.forEach(async (entry) => {
+      // We also need to consider when we touch two different files in a directory in different commits
+      if (commitsByDay[dateString].repoState[entry.path()] == undefined) {
+        if (entry.isFile()) {
+          commitsByDay[dateString].repoState[entry.path()] = {
+            entryName: "./" + entry.path(),
+            isDirectory: false,
+            children: null,
+          };
+        } else {
+          commitsByDay[dateString].repoState[entry.path()] = {
+            entryName: "./" + entry.path(),
+            isDirectory: true,
+            children: await getDirectoryEntries(entry),
+          };
+        }
+      }
+    });
   });
 
-  branchEventEmitter.on('end',  () => {
-    eventsEmitter.emit('FileComposed', contributors);
-  });
+  eventsEmitter.emit("FileComposed", commitsByDay);
 }
 
+// async function composeFile(branchEventEmitter) {
+
+//   const commitsByDay = {};
+//   // const contributors = {};
+//    /*
+//       This function gets all the commits and then creates an author object that stores information about which
+//       files this contributor has changed up to the most recent commit
+//     */
+//   branchEventEmitter.on("commit", async (commit) => {
+//     const commitDate = new Date(commit.date());
+//     // Reset the time to midnight to group by day
+//     commitDate.setHours(0, 0, 0, 0);
+//     const dateString = commitDate.toISOString();
+
+//     // Get Authors name
+//     const authorName = commit.committer().name();
+
+//     if (commitsByDay[dateString] == undefined) {
+//       commitsByDay[dateString] = {
+//         contributors: {},
+//         repoState: {}
+//       };
+//     }
+
+//     if (commitsByDay[dateString].contributors[authorName] == undefined) {
+//       commitsByDay[dateString].contributors[authorName] = {
+//         filesChanged: []
+//       };
+//     }
+
+//     const diffArray = await commit.getDiff();
+//     if (diffArray.length == 1) {
+
+//       const diff = diffArray[0];
+//       const patches = await diff.patches();
+//       const modifiedFiles = [];
+
+//       for (const patch of patches) {
+//         const newFile = patch.newFile().path();
+
+//         if (newFile) {
+//           commitsByDay[dateString].contributors[authorName].filesChanged.push(newFile);
+//         }
+//       }
+//     }
+
+//     /*
+//       This function below gets the current state of our Repo,
+//       Todo: We need to make sure we include
+//     */
+
+//     let currentCommitTreeEntires = await commit.getTree();
+//     let filenames = currentCommitTreeEntires.entries();
+
+//     filenames.forEach(async (entry) => {
+//       // We also need to consider when we touch two different files in a directory in different commits
+//       if (commitsByDay[dateString].repoState[entry.path()] == undefined) {
+//         if (entry.isFile()) {
+//           commitsByDay[dateString].repoState[entry.path()] = {
+//             entryName: "./" + entry.path(),
+//             isDirectory: false,
+//             children: null
+//           }
+//         } else {
+//           commitsByDay[dateString].repoState[entry.path()] = {
+//             entryName: "./" + entry.path(),
+//             isDirectory: true,
+//             children: await getDirectoryEntries(entry)
+//           }
+//         }
+//       }
+//     })
+//   });
+
+//   branchEventEmitter.on('end',  () => {
+//     eventsEmitter.emit('FileComposed', commitsByDay);
+//   });
+
+// }
+
 async function getDirectoryEntries(entry) {
-  const files = [];
+  const files = {};
   const directoryTree = await entry.getTree();
   const directoryEntries = directoryTree.entries();
   directoryEntries.forEach(async (entry) => {
     if (!entry.isFile()) {
-      files.push({
-        entryName: entry.name(),
+      files[entry.path()] = {
+        entryName: entry.path(),
         isDirectory: true,
-        children: await getDirectoryEntries(entry)
-      })
+        children: await getDirectoryEntries(entry),
+      };
     } else {
-      files.push({
-        entryName: entry.name(),
+      files[entry.path()] = {
+        entryName: entry.path(),
         isDirectory: false,
-        children: null
-      })
+        children: null,
+      };
     }
-  })
+  });
 
-  return files
+  return files;
 }
-
-
-
--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const app = express();
 const port = 4000;
 import cors from "cors";
 import gitAnalysis from "./gitAnalysis.js";
-import eventsEmitter from "./EventEmitter.js"
-
-
+import eventsEmitter from "./EventEmitter.js";
 
 const corsOptions = {
   origin: "*",
@@ -20,37 +18,30 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post(
-  "/uploadRepo",
-  upload.single("file"),
-  async (req, res, next) => {
-    res.setHeader("Cache-Control", "no-cache");
-    res.setHeader("Content-Type", "text/event-stream");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Connection", "keep-alive");
-    res.flushHeaders(); // flush the headers to establish SSE with client
+app.post("/uploadRepo", upload.single("file"), async (req, res, next) => {
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Connection", "keep-alive");
+  res.flushHeaders(); // flush the headers to establish SSE with client
 
-    const { path, originalname } = req.file;
-    console.log("File submitted", originalname);
-    let zip = new AdmZip(path);
-    zip.extractAllTo("repos");
-    let pathToRepo = `./repos/${originalname.replace(".zip", "")}`
-    
+  const { path, originalname } = req.file;
+  console.log("File submitted", originalname);
+  let zip = new AdmZip(path);
+  zip.extractAllTo("repos");
+  let pathToRepo = `./repos/${originalname.replace(".zip", "")}`;
 
-  
+  gitAnalysis(pathToRepo);
 
-    gitAnalysis(pathToRepo);
+  eventsEmitter.on("FileComposed", (file) => {
+    console.log("File Composed event!", file);
+  });
 
-    eventsEmitter.on("FileComposed", (file) => {
-      console.log('File Composed event!', file);
-    })
-
-    res.on("close", () => {
-      console.log("client dropped me");
-      res.end();
-    });
-  }
-);
+  res.on("close", () => {
+    console.log("client dropped me");
+    res.end();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
