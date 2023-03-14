c5564ef51f5bf1f70ac2445334762f4b20746df1
OmarDaSilva <72036965+OmarDaSilva@users.noreply.github.com>
2023-03-14 14:20:07 +0000

    return repo dates

--- gitAnalysis.js
+++ gitAnalysis.js
@@ -1,27 +1,91 @@
 import NodeGit from "nodegit";
-import eventsEmitter from "./EventEmitter.js";
-import fs from "fs";
-import path from "path";
-
+import { v4 as uuidv4 } from 'uuid';
 /*
       node colors:
       Dark blue - Directories
       Light blue - files
       Grey - contributors
-    */
+*/
 var darkblue = "darkblue";
 var lightBlue = "lightblue";
 var grey = "grey";
 
-export default async function gitAnalysis(repoFilepath, branchName) {
-  const repo = await NodeGit.Repository.open(repoFilepath);
+var localPath = './repos'
+
+export async function RepoDatesAnalysis(url, branchName) {
+  const repo = await NodeGit.Clone(url, localPath, {
+    fetchOpts: {
+      depth: null
+    }
+  })
+
+
   const branches = await getAllBranches(repo);
 
   const branch = branchName
     ? await repo.getBranch(branchName)
     : await repo.getBranch(branches.main);
 
-  const repoData = await composeJSONFile(repo, branch);
+  const repoData = await analysis(repo, branch);
+
+  return {
+    dates: repoData,
+    repoBranches: branches
+  }
+
+}
+
+export default async function gitAnalysis(repoFilepath, branchName, deltaDates) {
+  const repo = await NodeGit.Repository.open(repoFilepath);
+  const branches = await getAllBranches(repo);
+
+  const branch = branchName
+    ? await repo.getMasterCommit(branchName)
+    : await repo.getBranch(branches.main);
+
+  const repoData = await getRepoCommitDates(repo, branch);
+  return {
+    repoDates: repoData,
+    repoUrl: 'test'
+  };
+}
+
+async function getRepoCommitDates(repo, branch) {
+  const headCommit = await repo.getBranchCommit(branch);
+
+  const revWalk = repo.createRevWalk();
+  revWalk.sorting(NodeGit.Revwalk.SORT.REVERSE);
+  revWalk.push(headCommit.id());
+
+  const commits = await revWalk.getCommitsUntil((_commit) => true);
+
+  var commitsByDay = {};
+
+  await commits.reduce((promiseChain, item) => {
+    return promiseChain.then(
+      () =>
+        new Promise((resolve) => {
+          getCommitDates(item, resolve, commitsByDay);
+        })
+    );
+  }, Promise.resolve());
+
+  return commitsByDay  
+}
+
+async function getCommitDates(commit, cb, commitsByDay) {
+  const commitDate = new Date(commit.date());
+  // Reset the time to midnight to group by day
+  commitDate.setHours(0, 0, 0, 0);
+  const dateString = commitDate.toISOString();
+  let count = 0
+  if (commitsByDay[dateString] == undefined) {
+    commitsByDay[dateString] = {
+      indx: count++
+    };
+  }
+
+  cb();
 }
 
 async function getAllBranches(repo) {
@@ -39,7 +103,7 @@ async function getAllBranches(repo) {
   };
 }
 
-async function composeJSONFile(repo, branch) {
+async function analysis(repo, branch) {
   const headCommit = await repo.getBranchCommit(branch);
 
   const revWalk = repo.createRevWalk();
@@ -47,7 +111,8 @@ async function composeJSONFile(repo, branch) {
   revWalk.push(headCommit.id());
 
   const commits = await revWalk.getCommitsUntil((_commit) => true);
-  await composeData(commits);
+  let cleanData = await composeData(commits);
+  return cleanData;
 }
 
 async function processCommit(commit, cb, commitsByDay) {
@@ -94,12 +159,12 @@ async function processCommit(commit, cb, commitsByDay) {
   }
 
   /* 
-  //     This function below gets the current state of the Repo using the branch and commit,
-  //     it updates the state of the repo state on every commit, this is because we don't know
-  //     if the next commit will be the next day or not and that requires retriving the next commit.
-  //     it doesn't seem it's performance efficient but I haven't seen any siginifcant performance impact,
-  //     However, for potentially large repos this could be observed
-  //   */
+    This function below gets the current state of the Repo using the branch and commit,
+    it updates the state of the repo state on every commit, this is because we don't know
+    if the next commit will be the next day or not and that requires retriving the next commit.
+    it doesn't seem it's performance efficient but I haven't seen any siginifcant performance impact,
+    However, for potentially large repos this could be observed
+  */
 
   let currentCommitTreeEntires = await commit.getTree();
   let filenames = await currentCommitTreeEntires.entries();
@@ -139,46 +204,8 @@ async function composeData(commits) {
     );
   }, Promise.resolve());
 
-  dataProcessor.then(() => generateJSONFile(commitsByDay));
-}
-
-function generateJSONFile(data) {
-  /* 
-    We don't want to recreate a file if it already exists, there was
-    observed instances of commitsByDay being returned twice
-  */
-
-  const folderPath = "/GeneratedJSON";
-  const fileName = "data.json";
-
-  const checkFilePath = path.join(folderPath, fileName);
-  fs.access(checkFilePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      // lets generate the json file
-    } else {
-      // send existing json file to client
-    }
-  });
-
-  const cleanData = dataFormatter(data);
-  eventsEmitter.emit("FileGenerated", jsonString);
-
-  // Just send this string to the client
-  // const jsonString = JSON.stringify(data);
-  // const dirPath = path.join("./GeneratedJSON", "data");
-
-  // if (!fs.existsSync(dirPath)) {
-  //   fs.mkdirSync(dirPath);
-  // }
-
-  // const filePath = path.join(dirPath, "data.json");
-
-  // fs.writeFileSync(filePath, jsonString, "utf8", (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-  // });
+  const cleanData = await dataProcessor.then(() => dataFormatter(commitsByDay));
+  return cleanData;
 }
 
 function dataFormatter(data) {
@@ -193,52 +220,32 @@ function dataFormatter(data) {
     };
 
     const repoState = data[date].repoState;
-
+    const repoStateContributors = data[date].contributors;
     let currentTarget = "root";
-    // let group = 2;
 
-    traverseNodeLeafes(repoState, dataFormatted[date], currentTarget);
-    // for (const [pathName, value] of Object.entries(repoState)) {
-    //   if (!value.isDirectory) {
-    //     let node = {
-    //       id: pathName,
-    //       group: group,
-    //       colour: lightBlue,
-    //     };
-    //     let link = {
-    //       source: pathName,
-    //       target: currentTarget,
-    //       value: 1,
-    //     };
-    //     dataFormatted[date].nodes.push(node);
-    //     dataFormatted[date].links.push(link);
-    //   } else {
-    //     let node = {
-    //       id: pathName,
-    //       group: group,
-    //       colour: darkblue,
-    //     };
-    //     let link = {
-    //       source: pathName,
-    //       target: currentTarget,
-    //       value: 1,
-    //     };
-    //     dataFormatted[date].nodes.push(node);
-    //     dataFormatted[date].links.push(link);
-    //     traverseNodeLeafes(value.children, dataFormatted[date], pathName);
-    //   }
-    // }
+    traverseNodeLeafes(
+      repoState,
+      dataFormatted[date],
+      currentTarget,
+      repoStateContributors
+    );
   });
 
-  console.log(data);
+  return dataFormatted;
 }
 
-function traverseNodeLeafes(children, dataStore, parentNodePath) {
+function traverseNodeLeafes(
+  children,
+  dataStore,
+  parentNodePath,
+  repoStateContributors
+) {
   let newGroupNumber = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
   for (const [pathName, value] of Object.entries(children)) {
     if (!value.isDirectory) {
       let node = {
         id: pathName,
+        name: pathName,
         group: newGroupNumber,
         colour: lightBlue,
       };
@@ -249,9 +256,13 @@ function traverseNodeLeafes(children, dataStore, parentNodePath) {
       };
       dataStore.nodes.push(node);
       dataStore.links.push(link);
+      traverseContributions(repoStateContributors, pathName, dataStore);
+
+    
     } else {
       let node = {
         id: pathName,
+        name: pathName,
         group: newGroupNumber,
         colour: darkblue,
       };
@@ -262,7 +273,38 @@ function traverseNodeLeafes(children, dataStore, parentNodePath) {
       };
       dataStore.nodes.push(node);
       dataStore.links.push(link);
-      traverseNodeLeafes(value.children, dataStore, pathName);
+      traverseNodeLeafes(value.children, dataStore, pathName, repoStateContributors);
+    }
+  }
+}
+
+function traverseContributions(
+  repoStateContributors,
+  parentPathName,
+  dataStore
+) {
+  let newGroupNumber = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  if (repoStateContributors != undefined) {
+    for (const contributor of Object.keys(repoStateContributors)) {
+      if (repoStateContributors[contributor].filesChanged[parentPathName] != undefined) {
+        let uuid = uuidv4()
+        let contributorNode = {
+          id: `${contributor}-${uuid}`,
+          name: contributor,
+          group: newGroupNumber,
+          colour: grey
+        };
+  
+        let contributorLink = {
+          source: `${contributor}-${uuid}`,
+          target: parentPathName,
+          value: 1
+        }
+  
+        dataStore.nodes.push(contributorNode);
+        dataStore.links.push(contributorLink);
+  
+      }
     }
   }
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 4000;
 import cors from "cors";
 import gitAnalysis from "./gitAnalysis.js";
+import RepoDatesAnalysis from "./gitAnalysis.js"
 import eventsEmitter from "./EventEmitter.js";
 
 const corsOptions = {
@@ -18,35 +19,37 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/uploadRepo", upload.single("file"), async (req, res, next) => {
+app.post("/getRepoDeltaDates", upload.single("file"), async (req, res, next) => {
   res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Connection", "keep-alive");
-  res.flushHeaders(); // flush the headers to establish SSE with client
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Connection", "keep-alive");
+    res.flushHeaders();
 
+    const query = req.body.query;
+
+    switch (query) {
+      case query == "uploadRepo":
+        return 1;
+      case query == "analyseRepoDeltas":
+        return 2;
+    }
+
+})
+
+app.post("/uploadRepo", upload.single("file"), async (req, res, next) => {
   const { path, originalname } = req.file;
+  const token = req.body.token
+  const URL = req.body.URL
+  
   console.log("File submitted", originalname);
   let zip = new AdmZip(path);
   zip.extractAllTo("repos");
   let pathToRepo = `./repos/${originalname.replace(".zip", "")}`;
 
-  gitAnalysis(pathToRepo);
-
-  res.on("FileGenerated", (jsonString) => {
-    console.log("File Generated event!");
-    res.write(`data: ${jsonString}`)
-  })
-
-  eventsEmitter.on("FileGenerated", (jsonString) => {
-    console.log(jsonString)
-    res.write(jsonString);
-  });
-  
-  res.on("close", () => {
-    console.log("client dropped me");
-    res.end();
-  });
+  const repoAnalysed = await RepoDatesAnalysis(pathToRepo);
+  // const repoAnalysed = await gitAnalysis(pathToRepo);
+  res.json(repoAnalysed);
 });
 
 app.listen(port, () => {
